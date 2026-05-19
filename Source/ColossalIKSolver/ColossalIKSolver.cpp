81e74fec4fe1b9c7811325492e718b1380f4f18f
DESKTOP-K0P7O6M\Squibler <faisalibrahim610@gmail.com>
2026-05-19 17:40:26 -0500

    Inital commit to setup repo

---
+++ Source/ColossalIKSolver/ColossalIKSolver.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ColossalIKSolver.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ColossalIKSolver, "ColossalIKSolver" );
